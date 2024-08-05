6b744e7c26683a5cce19aeedc97bb337dc3f8f27
Yoon-Min <yoonmin0113@naver.com>
2024-08-05 17:54:35 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 88 ms, Memory: 12348 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.kt
@@ -0,0 +1,12 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val n = readln().toLong()
+    var sum = 0L
+    for(i in 1..n-1) {
+        sum += i
+    }
+    println(sum)
+    println(2)
+}
