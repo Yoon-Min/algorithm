d64d5c2ae085eaa921cb763e635758119bf6524f
Yoon-Min <yoonmin0113@naver.com>
2024-08-18 13:08:26 +0900

    [Bronze IV] Title: 베라의 패션, Time: 72 ms, Memory: 12352 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.math.*
+
+//val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val n = readln().toInt()
+    println(n * (n-1))
+}
