1d419accd4de9b14c0da2914426d461f09e6a4f9
Yoon-Min <yoonmin0113@naver.com>
2024-08-18 13:19:41 +0900

    [Bronze IV] Title: 녹색거탑, Time: 72 ms, Memory: 12320 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.math.*
+
+//val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val n = readln().toInt()
+    println(2.0.pow(n).toInt())
+}
