d97f5e51b848151d19024ae21fc80f80653e8302
Yoon-Min <yoonmin0113@naver.com>
2024-08-26 21:33:46 +0900

    [Bronze III] Title: 세 수, Time: 136 ms, Memory: 24284 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    println(readln().split(" ").map { it.toInt() }.sorted()[1])
+}
