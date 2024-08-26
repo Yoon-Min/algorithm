5b54685c6c61dd2d441636e73818b9c44650a228
Yoon-Min <yoonmin0113@naver.com>
2024-08-26 21:26:21 +0900

    [Bronze V] Title: A+B - 2, Time: 76 ms, Memory: 12344 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val a = readln().toInt()
+    val b = readln().toInt()
+    println(a+b)
+}
