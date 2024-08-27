7eef35659634c15717cbece9517287480a19dc80
Yoon-Min <yoonmin0113@naver.com>
2024-08-27 14:56:27 +0900

    [Bronze V] Title: 오늘 날짜, Time: 64 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    println("2024-08-27")
+}
