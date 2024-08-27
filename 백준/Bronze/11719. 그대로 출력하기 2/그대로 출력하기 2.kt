e37c0acaedd79d452a0facd5f4c9ed47c36b8482
Yoon-Min <yoonmin0113@naver.com>
2024-08-27 15:02:41 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 72 ms, Memory: 12584 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
@@ -0,0 +1,15 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+
+fun main() {
+    while(true) {
+        val line = br.readLine() ?: break
+        println(line)
+    }
+}
