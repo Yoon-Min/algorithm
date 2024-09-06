3e8ae529d491e07fe4aea8d145b9c99f3374d397
Yoon-Min <yoonmin0113@naver.com>
2024-09-06 14:44:53 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 64 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    println(
+        """
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+    """.trimIndent()
+    )
+}
+
