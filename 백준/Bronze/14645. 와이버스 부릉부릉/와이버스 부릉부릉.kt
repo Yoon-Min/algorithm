764acaa2169b4d1190984fd87b4d028cdfe0ffda
Yoon-Min <yoonmin0113@naver.com>
2024-10-05 23:12:22 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 108 ms, Memory: 20692 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.kt
@@ -0,0 +1,10 @@
+import kotlin.math.*
+
+fun main() {
+    val (n,k) = readln().split(" ").map { it.toInt() }
+    repeat(n) {
+        val (a,b) = readln().split(" ").map { it.toInt() }
+    }
+    println("비와이")
+}
+
