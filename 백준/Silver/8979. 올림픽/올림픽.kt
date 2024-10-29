813c30655b1468f5f9d951306b00ded668ee131d
Yoon-Min <yoonmin0113@naver.com>
2024-10-29 17:52:50 +0900

    [Silver V] Title: 올림픽, Time: 240 ms, Memory: 26588 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.kt
@@ -0,0 +1,38 @@
+import kotlin.math.*
+
+fun main() {
+    val (n, k) = readln().split(" ").map { it.toInt() }
+    val board = mutableListOf<Score>()
+    repeat(n) {
+        val (nation, g, s, b) = readln().split(" ").map { it.toInt() }
+        board.add(Score(nation, g, s, b))
+    }
+    board.sortWith(compareBy({it.gold}, {it.silver}, {it.bronze}))
+    board[0] = board.first().copy(rank = 1)
+    var prev = board.first()
+    for(i in 1..board.lastIndex) {
+        val cur = board[i]
+        if(cur.isEqual(prev)) {
+            board[i] = cur.copy(rank = prev.rank)
+        }
+        else {
+            board[i] = cur.copy(rank = prev.rank + 1)
+        }
+        prev = board[i]
+    }
+    println(board.find { it.nation == k}!!.rank)
+}
+
+data class Score(
+    val nation: Int,
+    val gold: Int,
+    val silver: Int,
+    val bronze: Int,
+    val rank: Int = -1
+) {
+    fun isEqual(other: Score): Boolean {
+        return gold == other.gold && silver == other.silver && bronze == other.bronze
+    }
+}
+
+
