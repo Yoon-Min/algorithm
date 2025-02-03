b98cc8309d3f01157272581555c8ba2277938e6c
Yoon-Min <yoonmin0113@naver.com>
2025-02-03 17:12:32 +0900

    [Gold V] Title: 달력, Time: 196 ms, Memory: 27988 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.kt
+++ 백준/Gold/20207. 달력/달력.kt
@@ -7,15 +7,21 @@ val dVector = listOf(Pair(0,1), Pair(1,0), Pair(0,-1), Pair(-1,0))
 fun main() {
     var areaSum = 0
     val n = readln().toInt()
-    val schedule = MutableList(366) { 0 }
+    val schedule = MutableList(400) { 0 }
     for (i in 0..<n) {
         val (s, e) = readln().split(" ").map { it.toInt() }
-        for(j in s..e) { schedule[j] += 1}
+        schedule[s] += 1
+        schedule[e+1] -= 1
     }
+//    println(schedule)
+    for(i in 1..schedule.lastIndex) {
+        schedule[i] += schedule[i-1]
+    }
+//    println(schedule)
 
     var continuousCount = 0
     var height = 0
-    for(i in 1..schedule.lastIndex) {
+    for(i in 1..365) {
         if(schedule[i] > 0) {
             continuousCount += 1
             height = max(height, schedule[i])
