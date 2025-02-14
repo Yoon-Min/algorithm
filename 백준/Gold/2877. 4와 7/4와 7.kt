58e9539180aef660db9eac11a7bf766dfe43177a
Yoon-Min <yoonmin0113@naver.com>
2025-02-14 15:32:58 +0900

    [Gold V] Title: 4와 7, Time: 140 ms, Memory: 21640 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.kt
+++ 백준/Gold/2877. 4와 7/4와 7.kt
@@ -17,8 +17,10 @@ fun main() {
     var midTopK = (s+e)/2
     var midBottomK = (s+e)/2 + 1
     var headerLength = 1
-    var midTop = top.slice(0 until headerLength) + '7'.copyUntil(length - headerLength)
-    var midBottom = bottom.slice(0 until headerLength) + '4'.copyUntil(length - headerLength)
+//    var midTop = top.slice(0 until headerLength) + '7'.copyUntil(length - headerLength)
+//    var midBottom = bottom.slice(0 until headerLength) + '4'.copyUntil(length - headerLength)
+    var midTop = getNextMidString(top, headerLength, '7')
+    var midBottom = getNextMidString(bottom, headerLength, '4')
 
     while(k != midTopK && k != midBottomK) {
         headerLength += 1
@@ -32,8 +34,10 @@ fun main() {
         }
         midTopK = (s+e)/2
         midBottomK = (s+e)/2 + 1
-        midTop = top.slice(0 until headerLength) + '7'.copyUntil(length - headerLength)
-        midBottom = bottom.slice(0 until headerLength) + '4'.copyUntil(length - headerLength)
+//        midTop = top.slice(0 until headerLength) + '7'.copyUntil(length - headerLength)
+//        midBottom = bottom.slice(0 until headerLength) + '4'.copyUntil(length - headerLength)
+        midTop = getNextMidString(top, headerLength, '7')
+        midBottom = getNextMidString(bottom, headerLength, '4')
     }
     if(midTopK == k) println(midTop)
     else println(midBottom)
@@ -41,5 +45,9 @@ fun main() {
 
 fun Char.copyUntil(length: Int): String {
     return List(length) { this }.joinToString("")
+}
+
+fun getNextMidString(origin: String, headerLength: Int, charForCopy: Char): String {
+    return origin.slice(0 until headerLength) + charForCopy.copyUntil(origin.length - headerLength)
 }
 
