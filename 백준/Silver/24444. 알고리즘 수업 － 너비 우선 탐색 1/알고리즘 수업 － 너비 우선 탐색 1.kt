fa02ded383ddab5ce6221f18ab3e30449819a0fe
Yoon-Min <yoonmin0113@naver.com>
2025-03-21 14:00:19 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1252 ms, Memory: 151112 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.kt
@@ -0,0 +1,32 @@
+import kotlin.math.*
+
+val linked = List(100001) { mutableListOf<Int>() }
+val seq = MutableList(100001) { 0 }
+var nextSeq = 1
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n,m,r) = readLine().split(" ").map { it.toInt() }
+    val q = ArrayDeque<Int>().also { it.add(r) }
+    val sb = StringBuilder()
+    seq[r] = nextSeq++
+    repeat(m) {
+        val (u,v) = readLine().split(" ").map { it.toInt() }
+        linked[u].add(v)
+        linked[v].add(u)
+    }
+    linked.forEach { it.sort() }
+
+    while(q.isNotEmpty()) {
+        val cur = q.removeFirst()
+        linked[cur].forEach { next ->
+            if(seq[next] == 0) {
+                seq[next] = nextSeq++
+                q.add(next)
+            }
+        }
+    }
+    repeat(n) {
+        sb.append("${seq[it+1]}\n")
+    }
+    println(sb)
+}
