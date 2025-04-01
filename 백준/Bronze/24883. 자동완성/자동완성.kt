215563b6551fcdb46d2ae9008b716dba3415a587
Yoon-Min <yoonmin0113@naver.com>
2025-04-01 22:25:20 +0900

    [Bronze V] Title: 자동완성, Time: 84 ms, Memory: 12204 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.kt
@@ -0,0 +1,9 @@
+import kotlin.math.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    when(readLine()) {
+        "N", "n" -> println("Naver D2")
+        else -> println("Naver Whale")
+    }
+}
+
